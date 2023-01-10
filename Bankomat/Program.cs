25fe86150f76561d7c1e540c06cbe237a3f17f04
LosCrimson <lukas.suslavicius@gmail.com>
2023-01-10 22:41:53 +0200

    Made the basic UI methods and implemented all the objects

--- Bankomat/Program.cs
+++ Bankomat/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bankomat.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,8 @@ namespace Bankomat
     {
         static void Main(string[] args)
         {
+            var i = new UIService();
+            i.MainMenu();
         }
     }
 }
